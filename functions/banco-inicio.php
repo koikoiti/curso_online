c1edc4056f58847faf5855c413cdb8824e5f5056
Israel <israelcbj@gmail.com>
2013-11-24 19:41:48 -0200

    mostrando ultimo acesso

--- functions/banco-inicio.php
+++ functions/banco-inicio.php
@@ -2,13 +2,13 @@
 	class bancoinicio extends banco{
 		
      	function BuscaUltimoAcesso($id){
-        	$Sql = 'SELECT * FROM c_acesso where idusuario = "'.$id.'" order by DESC limit 1,1 ';
-            echo $Sql;die;
+        	$Sql = 'SELECT * FROM c_acesso where idusuario = "'.$id.'" order by data DESC limit 1,1 ';
+            
         	$result = parent::Execute($Sql);
         	$num_rows = parent::Linha($result);
         	   if($num_rows){
     			 $rs = mysql_fetch_array($result , MYSQL_ASSOC);
-                 return $rs['acesso'];
+                 return $rs['data'];
                 }
         }
   }
--- lib/inicio.php
+++ lib/inicio.php
@@ -6,7 +6,10 @@
 	$banco = new bancoinicio();
     
     #Busca Ultimo Acesso
-    $acesso = BuscaUltimoAcesso($_SESSION['id']);
+    $acesso = $banco->BuscaUltimoAcesso($_SESSION['id']);
+    $aux = explode(' ',$acesso);
+    $data = date("d/m/Y", strtotime($aux[0]));
+    $acesso = $data.' '.$aux[1];
 
 	#Imprimi valores
 	$Conteudo = $banco->CarregaHtml('inicio');
