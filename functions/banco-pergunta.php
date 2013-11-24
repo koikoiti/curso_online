19e7c8dc083cf4755a77980a395b97a15def22d9
Koiti-Note <evandrokoiti@gmail.com>
2013-11-24 16:20:18 -0200

    perguntas e respostas

---
+++ functions/banco-pergunta.php
@@ -0,0 +1,25 @@
+<?php
+    class bancopergunta extends banco{
+        
+        function MontaPergunta($idpergunta){
+            $Sql = "SELECT pergunta FROM c_perguntas WHERE idpergunta = " . $idpergunta;
+            $result = parent::Execute($Sql);
+            $rs = mysql_fetch_array($result, MYSQL_ASSOC);
+            return $rs["pergunta"];
+        }#Fim MontaPergunta
+        
+        function MontaRespostas($idpergunta){
+            $Auxilio = parent::CarregaHtml('itens/respostas-itens');
+            $Sql = "SELECT * FROM c_respostas WHERE idpergunta = " . $idpergunta;
+            $result = parent::Execute($Sql);
+            while($rs = mysql_fetch_array($result, MYSQL_ASSOC)){
+                $Linha = $Auxilio;
+                $Linha = str_replace("<%IDRESPOSTA%>", $rs["idresposta"], $Linha);
+                $Linha = str_replace("<%IDPERGUNTA%>", $idpergunta, $Linha);
+                $Linha = str_replace("<%RESPOSTA%>", $rs["resposta"], $Linha);
+                $Respostas .= $Linha;
+            }
+            return $Respostas;
+        }#Fim MontaRespostas
+    }#Fim classe
+?>
--- lib/pergunta.php
+++ lib/pergunta.php
@@ -1,10 +1,21 @@
 <?php
 	#include das funcoes da tela inico
-	include('functions/banco-inicio.php');
+	include('functions/banco-pergunta.php');
 
 	#Instancia o objeto
-	$banco = new bancoinicio();
-
+	$banco = new bancopergunta();
+    
+    #Pega idpergunta da URL
+    $idpergunta = $this->PaginaAux[0];
+    
+    #Monta pergunta
+    $pergunta = $banco->MontaPergunta($idpergunta);
+    
+    #Monta as respostas da pergunta
+    $Respostas = $banco->MontaRespostas($idpergunta);
+    
 	#Imprimi valores
 	$Conteudo = $banco->CarregaHtml('pergunta');
+    $Conteudo = str_replace("<%PERGUNTA%>", utf8_decode($pergunta), $Conteudo);
+    $Conteudo = str_replace("<%RESPOSTAS%>", utf8_decode($Respostas), $Conteudo);
 ?>
