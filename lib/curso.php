83952eec81dd1625352dc0dc48f5c50eff86bfdc
Israel <israelcbj@gmail.com>
2013-11-24 21:01:40 -0200

    kkkkk

--- lib/curso.php
+++ lib/curso.php
@@ -436,14 +436,6 @@
             ';
     }
     
-    if($this->PaginaAux[0] == 'zerar'){
-        $Sql = "UPDATE c_usuarios SET r1=0, r2=0, r3=0, r4=0, r5=0, r6=0, r7=0, r8=0, r9=0, r10=0 WHERE idusuario = " . $_SESSION["id"];
-        $result = $banco->Execute($Sql);
-        if($result){
-            $banco->RedirecionaPara('inicio');
-        }
-    }
-    
 	#Imprimi valores
 	$Conteudo = $banco->CarregaHtml('curso');
     $Conteudo = str_replace('<%CURSO%>',$descr,$Conteudo);
