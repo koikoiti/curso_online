3d092328fc536151285ed6f815f3f65b38aabb77
Israel <israelcbj@gmail.com>
2013-11-24 20:12:07 -0200

    fix inicio

--- functions/banco-inicio.php
+++ functions/banco-inicio.php
@@ -12,9 +12,32 @@
             }
         }
         
-        function BuscaQuestoes($id){
-            
-            
-        }
+        function MontaQ(){
+            $Sql = "SELECT * FROM c_usuarios WHERE idusuario = ". $_SESSION["id"];
+            $result = parent::Execute($Sql);
+            $rs = mysql_fetch_array($result, MYSQL_ASSOC);
+            for($i = 1; $i < 11; $i++){
+                $coluna = "r" . $i;
+                if($rs[$coluna] == 0){
+                    $falta[] = $i;
+                }
+            }
+            if($falta){
+                $total = count($falta);
+                $cont = 1;
+                $Resultado = "Oops,ainda falta você responder a(s) pergunta(s): ";
+                foreach($falta as $f){
+                    if($cont == $total){
+                        $Resultado .= $f. ".";
+                    }else{
+                        $Resultado .= $f . ",";
+                    }
+                    $cont++;
+                }
+            }else{
+                #Calcula resultado
+            }
+            return $Resultado;
+        }#Fim MontaResultado
   }
 ?>
--- lib/inicio.php
+++ lib/inicio.php
@@ -12,10 +12,19 @@
     $acesso = $data.' '.$aux[1];
     
     #Busca quantas questoes falta
-    $questoes = $banco->BuscaQuestoes($_SESSION['id']);
+    $questoes = $banco->MontaQ($_SESSION['id']);
+    if($questoes == NULL){
+        $questoes = 'Curso Concluído.';
+        $cor = 'green';
+    }else{
+        $cor = 'red';
+    }
     
 
 	#Imprimi valores
 	$Conteudo = $banco->CarregaHtml('inicio');
     $Conteudo = str_replace('<%ACESSO%>',$acesso,$Conteudo);
+    $Conteudo = str_replace('<%Q%>',$questoes,$Conteudo);
+    $Conteudo = str_replace('<%COR%>',$cor,$Conteudo);
+    $Conteudo = str_replace('<%USUARIO%>',$_SESSION['usuario'],$Conteudo);
 ?>
